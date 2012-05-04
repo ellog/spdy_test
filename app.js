13f6ff5cafb00c1d28e21713bb6b1e91cb0388b5
小松 健作 <kensaku.komatsu@gmail.com>
2012-05-04 14:09:27 +0900

    improve mimeType detection and enable regardless of SPDY support

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 var spdy = require('spdy'),
     fs = require('fs');
 
+// implement last() for Array
+// Ref : http://stackoverflow.com/questions/3235043/last-element-of-array-in-javascript
+if(!Array.prototype.last) {
+  Array.prototype.last = function() {
+    return this[this.length - 1];
+  }
+}
+
 var options = {
   key: fs.readFileSync(__dirname + '/keys/spdy-key.pem'),
   cert: fs.readFileSync(__dirname + '/keys/spdy-cert.pem'),
   ca: fs.readFileSync(__dirname + '/keys/spdy-csr.pem')
 };
 
-var server = spdy.createServer(options, function(req, res) {
-  if(!!req.isSpdy === false ) {
-    res.writeHead(200);
-    res.end("Your browser doesn't support SPDY");
-    return;
-  }
-  var url_ = req.client.headers.url.split(/[?#]/)[0];
+var mimeTypes = {
+  "png" : "image/png"
+  , "html" : "text/html"
+  , "css" : "text/css"
+  , "js" : "application/javascript"
+  , "txt" : "text/plain"
+}
 
-  // console.log("streamID: "+req.streamID + ", url: "+url_);
+var server = spdy.createServer(options, function(req, res) {
+  var url_ = req.url.split(/[?#]/)[0];
+
+  console.log("url: "+url_);
 
   if(url_ === "/") {
+    // Displays current transfer mode
     var mesg_ = req.isSpdy ? "Hello SPDY!" : "Hello HTTPS"
     res.writeHead(200);
 
     res.end(mesg_);
   } else {
+    // Generic Web Server enhanced w/ SPDY technology
     var file_ = __dirname + "/public" + url_;
     fs.readFile(file_, function(err, data) {
       if(!!err === false) {
-        var contentType = null;
-        if(url_.match(/png$/)) {
-          contentType = "image/png";
-        } else if (url_.match(/html$/)) {
-          contetnType = "text/html";
-        } else if (url_.match(/css$/)) {
-          contetnType = "text/css";
-        } else if (url_.match(/js$/)) {
-          contetnType = "application/javascript";
-        } else {
-          contetnType = "text/plain";
-        }
+        var ext = url_.split(".").last()
+          , contentType = mimeTypes.hasOwnProperty(ext) ? mimeTypes[ext] : "text/plain";
+
         res.writeHead(200, {
           "Content-Type": contentType,
           "Content-Length" : data.length }
